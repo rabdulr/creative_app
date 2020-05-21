f0fc1e18e57954981f6844cb405b1c94fb208e1c
Redzuan <rabdulr@icloud.com>
2020-05-21 15:25:03 -0700

    First Commit

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Main = () => {
+    return(
+        <div>
+            <button>Sign Up</button>
+            <h1>Hello World</h1>
+            <form>
+                <label>First Name: <input text></input></label>
+                <label>Last Name: <input text></input></label>
+                <label>Email: <input text></input></label>
+                <label>Hopes: <input type='text'></input></label>
+            </form>
+        </div>
+    )
+}
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Main from './Main.js'
+
+const root = document.querySelector('#root');
+
+ReactDom.render(<Main />, root);
